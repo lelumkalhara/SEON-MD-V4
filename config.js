3427d61052eb99313cda870868b1422f831c7962
lelumkalhara <lelumkalhara44@gmail.com>
2025-02-26 22:10:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '', //Put Session-id Here
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || '', //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '', //Put Your Github Auth Token
+SESSION_ID: process.env.SESSION_ID || 'SEON-MD=JIgT3awY#2vTQ-DXXg8GDkmq9qIQ7DMwlyAuRrx-fQnSY7adSeTU', //Put Session-id Here
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || 'uvindugit', //Put Your Gitub Username
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || 'jjs04aXnF4kFaZ1siVExodAyS2Qirw4fUUXI', //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || 'SEON-MD-DATABASE' //Bot Database Repo Name
 };
